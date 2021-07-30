39b174cba70a2e98d67df0339965c126b28626c0
igah-franklin <igahfranklyn@gmail.com>
2021-07-30 01:05:37 -0700

    toggle password

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+
+// togglePassword.addEventListener('click', toggleFunc)
+// function toggleFunc(){
+//     if(password.type === 'password'){
+//         password.type = 'text'
+//     }else{
+//         password.type = 'password'
+//     }
+// }
+
+togglePassword.addEventListener('click', () =>{
+    password.type === 'password' 
+    ? password.type = 'text' 
+    : password.type = 'password'
+
+})
+
+
